3c022c753a72876eaf6acc3e02fb316cb189eaa8
Lorderot <Ihorkayf_@mail.ru>
2018-01-21 16:37:56 +0200

    Fixed AuthGuard

--- QuoteSocialNetwork.WEB/src/app/auth.guard.ts
+++ QuoteSocialNetwork.WEB/src/app/auth.guard.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 import {Constants } from './shared/constants';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
     constructor(
         private router: Router,
-        private firebase: AngularFireAuth
+        private firebase: AngularFireAuth,
+        private auth: AngularFireAuth
     ) { }
 
-    canActivate() {
-        if (this.firebase.auth.currentUser
-            && !this.firebase.auth.currentUser.isAnonymous) {
-            // logged in so return true
-            return true;
-        }
-
-        // not logged in so redirect to login page
-        this.router.navigate(['/login']);
-        return false;
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        return this.firebase.authState
+                            .take(1)
+                            .map(u => {
+                                return u && !u.isAnonymous;
+                            })
+                            .do(authenticated => {
+                                if (!authenticated) {
+                                    // not logged in so redirect to login page
+                                    this.router.navigate(['/login']);
+                                }
+                            });
     }
 }
