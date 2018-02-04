8c7e81e79467fd2c317fa18f724d42d3d8d3a4f0
olecsiuyae <olecsiuyae@gmail.com>
2018-02-04 20:28:40 +0200

    Add forms/android project with navigation

---
+++ QuoteSocialNetwork.Mobile/QSN/QSN/QSN/View/MasterPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using QSN.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QSN.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : ContentPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+            BindingContext = new MasterViewModel();
+        }
+    }
+}
---
+++ QuoteSocialNetwork.Mobile/QSN/QSN/QSN/ViewModel/MasterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using QSN.View;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace QSN.ViewModel
+{
+    public class MasterViewModel
+    {
+        public ICommand NavigationCommand
+        {
+            get
+            {
+                return new Command((value) =>
+                {
+                    var mdp = (Application.Current.MainPage as MasterDetailPage);
+                   
+                    // Hide the Master page
+                    mdp.IsPresented = false;
+
+                    switch (value)
+                    {
+                        case "1":
+                            mdp.Detail = new NavigationPage(new QuotesPage());
+                            break;
+                        case "2":
+                            mdp.Detail = new NavigationPage(new GroupsPage());
+                            break;
+                    }
+
+                });
+            }
+        }
+    }
+}
